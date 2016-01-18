f948c43d3e47383945fd1f3511cd09e5a90ed7d7
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-18 17:04:47 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,24 +226,16 @@ function create_config( )
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@FakeAdmin [Founder][Developer][Help&Support]
+@alirezaasadi82 [Sponser][Help&Support]
+@Persian_Dewil [Help&Support]
 
 Special thanks to
 awkward_potato
 Siyanew
 topkecleon
 Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -324,105 +316,104 @@ Commands list :
 
 !kick [username|id]
 You can also do it by reply
-
+〰〰〰〰〰〰
 !ban [ username|id]
 You can also do it by reply
-
+〰〰〰〰〰〰
 !unban [id]
 You can also do it by reply
-
+〰〰〰〰〰〰
 !who
 Members list
-
+〰〰〰〰〰〰
 !modlist
 Moderators list
-
+〰〰〰〰〰〰
 !promote [username]
 Promote someone
-
+〰〰〰〰〰〰
 !demote [username]
 Demote someone
-
+〰〰〰〰〰〰
 !kickme
 Will kick user
-
+〰〰〰〰〰〰
 !about
 Group description
-
+〰〰〰〰〰〰
 !setphoto
 Set and locks group photo
-
+〰〰〰〰〰〰
 !setname [name]
 Set group name
-
+〰〰〰〰〰〰
 !rules
 Group rules
-
+〰〰〰〰〰〰
 !id
 return group id or user id
-
+〰〰〰〰〰〰
 !help
-
-!lock [member|name|bots|leave]	
-Locks [member|name|bots|leaveing] 
-
-!unlock [member|name|bots|leave]
-Unlocks [member|name|bots|leaving]
-
+This help text
+〰〰〰〰〰〰
+!lock [member|name|bots|leave|arabic]	
+Locks [member|name|bots|leaveing|arabic] 
+〰〰〰〰〰〰
+!unlock [member|name|bots|leave|arabic]
+Unlocks [member|name|bots|leaving|arabic]
+〰〰〰〰〰〰
 !set rules <text>
 Set <text> as rules
-
+〰〰〰〰〰〰
 !set about <text>
 Set <text> as about
-
+〰〰〰〰〰〰
 !settings
 Returns group settings
-
+〰〰〰〰〰〰
 !newlink
 create/revoke your group link
-
+〰〰〰〰〰〰
 !link
 returns group link
-
+〰〰〰〰〰〰
 !owner
 returns group owner id
-
+〰〰〰〰〰〰
 !setowner [id]
 Will set id as owner
-
+〰〰〰〰〰〰
 !setflood [value]
 Set [value] as flood sensitivity
-
+〰〰〰〰〰〰
 !stats
 Simple message statistics
-
+〰〰〰〰〰〰
 !save [value] <text>
 Save <text> as [value]
-
+〰〰〰〰〰〰
 !get [value]
 Returns text of [value]
-
+〰〰〰〰〰〰
 !clean [modlist|rules|about]
 Will clear [modlist|rules|about] and set it to nil
-
+〰〰〰〰〰〰
 !res [username]
 returns user id
 "!res @username"
-
+〰〰〰〰〰〰
 !log
 will return group logs
-
+〰〰〰〰〰〰
 !banlist
 will return group ban list
-
-**U can use both "/" and "!" 
-
-
+〰〰〰〰〰〰
+**U can use "/" and "!"  and "."
+〰〰〰〰〰〰
 *Only owner and mods can add bots in group
-
-
+〰〰〰〰〰〰
 *Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
-
+〰〰〰〰〰〰
 *Only owner can use res,setowner,promote,demote and log commands
 
 ]]
