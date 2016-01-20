92d8f615ef21c2861234f44e9f732050805fe875
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-20 23:25:22 +0330

    Update block.lua

--- plugins/block.lua
+++ plugins/block.lua
@@ -1,16 +1,16 @@
 local function run(msg, matches)
     if matches[1] == "block" then
-	if not is_sudo then
-	return "Only Sudo Can Block Users"
-	end
+	if not is_sudo(msg) then
+        return "Only Sudo Can Block Users!!"
+      end
 	local user_id = "user#id"..matches[2]
 	block_user(user_id, ok_cb, false)
 	return "*User Has Been Blocked*"
     end
     if matches[1] == "unblock" then
-	if not is_sudo then
-	return "Only Sudo Can Unblock Users"
-	end
+	if not is_sudo(msg) then
+        return "Only Sudo Can Unblock Users!!"
+      end
 	local user_id = "user#id"..matches[2]
 	unblock_user(user_id, ok_cb, false)
 	return "*User Has Been Unblocked*"
