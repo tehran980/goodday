fcdab3ff21540f268fe742a3a2613a690f2de998
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-20 23:15:15 +0330

    Update block.lua

--- plugins/block.lua
+++ plugins/block.lua
@@ -1,10 +1,12 @@
 local function run(msg, matches)
     if matches[1] == "block" then
 	local user_id = "user#id"..matches[2]
+	return "*User Has Been Blocked*"
 	block_user(user_id, ok_cb, false)
     end
     if matches[1] == "unblock" then
 	local user_id = "user#id"..matches[2]
+	return "User Has Been Unblocked"
 	unblock_user(user_id, ok_cb, false)
     end
 end
