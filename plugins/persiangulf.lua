104f2efb2bc3cdf9f73992314a367c9e060531d1
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-22 21:59:39 +0330

    Update persiangulf.lua

--- plugins/persiangulf.lua
+++ plugins/persiangulf.lua
@@ -8,6 +8,14 @@ local function run(msg, matches)
       send_document(receiver, "./bot/sticker.webp", ok_cb, false)
 end
 end
+end
+end
+end
+end
+end
+end
+end
+
 return {
   description = ":D",
   usage = {
