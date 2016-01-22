056914fe70892dee4f1dec6a44c82dfe124d9bc6
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-22 22:00:58 +0330

    Update persiangulf.lua

--- plugins/persiangulf.lua
+++ plugins/persiangulf.lua
@@ -5,16 +5,10 @@ local function run(msg, matches)
   local about = 'PersianGulfBot\nThe New Way To Manage Your Groups\n\nAdmins\n@FakeAdmin [Founder][Developer][Help&Support]\n@alirezaasadi82 [Sponser][Help&Support]\n@Persian_Dewil [Help&Support]'
 	 if matches[1] == 'persiangulf' then
 	 return about
+	 end
       send_document(receiver, "./bot/sticker.webp", ok_cb, false)
 end
-end
-end
-end
-end
-end
-end
-end
-end
+
 
 return {
   description = ":D",
