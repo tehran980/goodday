3e09436f50f3dfe37bc56737ab1fcf1d204e1c22
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-23 16:50:01 +0330

    Update persiangulf.lua

--- plugins/persiangulf.lua
+++ plugins/persiangulf.lua
@@ -2,11 +2,6 @@ do
 
 local function run(msg, matches)
   local receiver = get_receiver(msg)
-  local about = 'PersianGulfBot\nThe New Way To Manage Your Groups\n\nAdmins\n@FakeAdmin [Founder][Developer][Help&Support]\n@alirezaasadi82 [Sponser][Help&Support]\n@Persian_Dewil [Help&Support]'
-	 if matches[1] == 'persiangulf' then
-	 return about
-	 end
-  local receiver = get_receiver(msg)
 	 if matches[1] == 'persiangulf' then
       send_document(receiver, "./bot/sticker.webp", ok_cb, false)
 end
