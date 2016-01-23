f302eaa85c7c8ef22e40c232e02c6d1300ee517f
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-23 21:14:44 +0330

    Update fosh.lua

--- plugins/fosh.lua
+++ plugins/fosh.lua
@@ -1,11 +1,7 @@
 do
 
 function run(msg, matches)
-if msg.text == "be_(.*)_fosh_bede" then
 	return "کیرم تو کس ننت " .. matches[1]
-end
-if msg.text == "be_(.*)_salam_kon" then
-	return "سلام " .. matches[1]
 end
 end
 return {
