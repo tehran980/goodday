a8596687e0ff8ff532e12760b1f49b0e8d72265c
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-23 18:00:48 +0330

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -1,6 +1,6 @@
 local function run(msg)
   if is_admin(msg) then
-  local about = AdminS :\n   @alirezaasadi_psg [Sponser&Manager] \n@Mrhalix [Founder&Developer]\n@Persian_Dewil [Assistant&Helper]\nBuilt to the 🇮🇷Persian Gulf🇮🇷\n📡 ID channel : 📡\n📡 https://telegram.me/persian_gulf_team 📡\n❤️ Thank You ❤️
+  local about = 'AdminS :\n   @alirezaasadi_psg [Sponser&Manager] \n@Mrhalix [Founder&Developer]\n@Persian_Dewil [Assistant&Helper]\nBuilt to the 🇮🇷Persian Gulf🇮🇷\n📡 ID channel : 📡\n📡 https://telegram.me/persian_gulf_team 📡\n❤️ Thank You ❤️'
         return about
     end
 end
