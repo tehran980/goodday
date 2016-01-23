3fde0eb3bf34450056ad9c7f327f98b210cd57c5
aminaleahmad <aminaleahmad@yahoo.com>
2016-01-23 17:30:34 +0330

    Create about.lua

---
+++ plugins/about.lua
@@ -0,0 +1,12 @@
+local function run(msg)
+  if is_admin(msg) then
+  local about = AdminS :\n   @alirezaasadi_psg [Sponser&Manager] \n@Mrhalix [Founder&Developer]\n@Persian_Dewil [Assistant&Helper]\nBuilt to the 🇮🇷Persian Gulf🇮🇷\n📡 ID channel : 📡\n📡 https://telegram.me/persian_gulf_team 📡\n❤️ Thank You ❤️
+        return about
+    end
+end
+return {patterns = {
+   "!about",
+   "about",
+   "درباره",
+   "!درباره",
+    }, run = run}
