51dba2a263a5bbb842049b73e6bf5a702e8544f0
tgcloner <aminaleahmad10@gmail.com>
2016-01-24 20:23:01 +0330

    Update lock_join.lua

--- plugins/lock_join.lua
+++ plugins/lock_join.lua
@@ -6,9 +6,9 @@ local function run(msg)
     if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
   if not is_momod(msg) then
   chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msg = 'Join link is Locked !'
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
+   -- local msg = 'Join link is Locked !'
+  -- local receiver = msg.to.id
+   -- send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
             end
         end
       end
