e0d015f08b1995e402406094b0849568d3d65e2a
tgcloner <aminaleahmad10@gmail.com>
2016-01-24 19:21:06 +0330

    Create fwd.lua

---
+++ plugins/fwd.lua
@@ -0,0 +1,10 @@
+local function run(msg)
+    if not is_momod(msg) then
+     local msg =  'msg#id'..msg.id
+        fwd('chat#id'..msg.to.id, ..msg, ok_cb, true)
+    end
+end
+
+return {patterns = {
+    "fwd",
+    }, run = run}
