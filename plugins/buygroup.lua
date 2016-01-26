ec87442ca0386f1c55c3b9d9ba4088bb882df8e1
tgcloner <aminaleahmad10@gmail.com>
2016-01-26 19:58:58 +0330

    Update buygroup.lua

--- plugins/buygroup.lua
+++ plugins/buygroup.lua
@@ -1,6 +1,6 @@
 local function run(msg)
     local buy = 'For buy a group go 1 of this ids : \n @Mrhalix \n\n @alirezaasadi_psg Those ids Reported pls send 10 msg :D'
-     local buy = '..buy..\nبرای خرید گروه به آیدی های زیر بروید!!\n @Mrhalix\n\n@alirezaasadi_psg\nدو آیدی ریپورت هستند'
+     local buy = ..buy..'\nبرای خرید گروه به آیدی های زیر بروید!!\n @Mrhalix\n\n@alirezaasadi_psg\nدو آیدی ریپورت هستند'
 return buy
 end
 return {patterns = {
