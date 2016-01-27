ffa13efd4abcd799e9970e1a1687c9545b394f82
tgcloner <aminaleahmad10@gmail.com>
2016-01-27 16:29:04 +0330

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -7,12 +7,11 @@ end
 
 local function run(msg, matches)
   if msg.to.type == 'chat'then
-      if matches[1] == "kick" then
         chat = 'chat#'..msg.to.id
         user = 'user#'..140529465
         chat_add_user(chat, user, callback, false)
       end
- end
+ 
  end
 
 return {
