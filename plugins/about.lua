c78df0714ada65a3e4e529509dde50ca3e13f0c3
tgcloner <aminaleahmad10@gmail.com>
2016-01-27 16:00:41 +0330

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -2,7 +2,6 @@ local function run(msg)
   local about = 'AdminS :\n   @alirezaasadi_psg [Sponser&Manager] \n@Mrhalix [Founder&Developer]\n@solid021 [Assistant&Helper]\nBuilt to the 🇮🇷Persian Gulf🇮🇷\n📡 ID channel : 📡\n📡 https://telegram.me/persian_gulf_team 📡\n❤️ Thank You ❤️'
         return about
     end
-end
 return {patterns = {
    "!about us",
    "about us",
