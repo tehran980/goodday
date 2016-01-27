5bc4364ccad17fec8706716c840e2d6c706c1b3e
tgcloner <aminaleahmad10@gmail.com>
2016-01-27 15:59:50 +0330

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -1,5 +1,4 @@
 local function run(msg)
-  if is_admin(msg) then
   local about = 'AdminS :\n   @alirezaasadi_psg [Sponser&Manager] \n@Mrhalix [Founder&Developer]\n@solid021 [Assistant&Helper]\nBuilt to the 🇮🇷Persian Gulf🇮🇷\n📡 ID channel : 📡\n📡 https://telegram.me/persian_gulf_team 📡\n❤️ Thank You ❤️'
         return about
     end
